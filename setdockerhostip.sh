df42ef60a521a4642051c15824d835e0b3a0b61d
ktomakh <ktomakh@mirantis.com>
2018-03-27 14:55:13 +0300

    added script setdockerhostip.sh

---
+++ setdockerhostip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DOCKER_HOST_IP="192.168.122.110"
+find alertmanager -type  f -name \* -exec sed -i -r 's/docker_host_ip/'$DOCKER_HOST_IP'/g' {} \;
+find prometheus -type f -name \* -exec sed -i -r 's/docker_host_ip/'$DOCKER_HOST_IP'/g' {} \;
+find telegraf -type f -name \* -exec sed -i -r 's/docker_host_ip/'$DOCKER_HOST_IP'/g' {} \;
+
